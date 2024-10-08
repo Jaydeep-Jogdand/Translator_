18d4255031b46aa2966276a13b0fa4d555c6fc33
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-08 11:56:46 +0530

    Update Point.cpp

--- StlToDatTranslator/Source File/Point.cpp
+++ StlToDatTranslator/Source File/Point.cpp
@@ -1,6 +1,6 @@
 #include "Point.h"
 
-Point::Point(double x, double y, double z)
+Point::Point(int x, int y, int z)
     : xPos(x), yPos(y), zPos(z) {}  // Correct placement of the initialization list
 Point::~Point()
 {
