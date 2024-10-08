470c94cfd2099e95245e7564aeb3f163af83ffc6
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-08 15:49:13 +0530

    Update Writer.cpp

--- StlToDatTranslator/Source File/Writer.cpp
+++ StlToDatTranslator/Source File/Writer.cpp
@@ -9,7 +9,7 @@ Writer::Writer() {}
 void Writer::saveToFile(const std::string& filePath, const std::vector<Triangle>& shapeTriangles) {
     std::ofstream file(filePath);
     for (const auto& tri : shapeTriangles) {
-        //file << std::fixed /*<< std::setprecision(6)*/
+   
            file << tri.vertex1.getX() << " " << tri.vertex1.getY() << " " << tri.vertex1.getZ() << "\n"
             << tri.vertex2.getX() << " " << tri.vertex2.getY() << " " << tri.vertex2.getZ() << "\n"
             << tri.vertex3.getX() << " " << tri.vertex3.getY() << " " << tri.vertex3.getZ() << "\n"
