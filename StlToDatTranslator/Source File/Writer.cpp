2d3af77c4d527717502061915c27ea7a06b023e1
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-08 15:48:17 +0530

    Update Writer.cpp

--- StlToDatTranslator/Source File/Writer.cpp
+++ StlToDatTranslator/Source File/Writer.cpp
@@ -1,29 +1,4 @@
-//#include "Writer.h"
-//#include <fstream>
-//#include <iomanip>
-//#include <iostream>
-//
-//
-//Writer::Writer()
-//{
-//
-//}
-//
-//void Writer::saveToFile(const std::string& filePath, const std::vector<Triangle>& shapeTriangles) {
-//    std::ofstream file(filePath);
-//    for (const auto& tri : shapeTriangles) {
-//        file << std::fixed << std::setprecision(6)
-//            << tri.vertex1.xPos << " " << tri.vertex1.yPos << " " << tri.vertex1.zPos << "\n"
-//            << tri.vertex2.xPos << " " << tri.vertex2.yPos << " " << tri.vertex2.zPos << "\n"
-//            << tri.vertex3.xPos << " " << tri.vertex3.yPos << " " << tri.vertex3.zPos << "\n"
-//            << tri.vertex1.xPos << " " << tri.vertex1.yPos << " " << tri.vertex1.zPos << "\n";
-//    }
-//    std::cout << "Triangles saved to file: " << shapeTriangles.size() << std::endl; // Debug print
-//}
-//Writer::~Writer()
-//{
-//
-//}
+
 #include "Writer.h"
 #include <fstream>
 #include <iomanip>
