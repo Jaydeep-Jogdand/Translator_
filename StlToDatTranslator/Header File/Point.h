7bb2f84e0ef00a130241daf1fbd31f6e9c39cc3d
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-08 11:56:05 +0530

    Update Point.h

--- StlToDatTranslator/Header File/Point.h
+++ StlToDatTranslator/Header File/Point.h
@@ -2,7 +2,10 @@
 
 class Point {
 public:
-    double xPos, yPos, zPos;
-    Point(double x, double y, double z);
+    // double xPos, yPos, zPos;
+    // Point(double x, double y, double z);
+
+    int xPos, yPos, zPos;
+    Point(int x, int y, int z);
     ~Point();
 };
