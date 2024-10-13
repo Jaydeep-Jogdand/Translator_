07b315e435df02c910de7535a891e223af5a89c4
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-13 18:09:27 +0530

    Update Triangle.cpp
    Add Getter method

--- StlToDatTranslator/Source File/Triangle.cpp
+++ StlToDatTranslator/Source File/Triangle.cpp
@@ -1,8 +1,19 @@
+
 #include "Triangle.h"
 
 Triangle::Triangle(Point vertex1, Point vertex2, Point vertex3)
     : vertex1(vertex1), vertex2(vertex2), vertex3(vertex3) {}
-Triangle::~Triangle(){}
 
+Point Triangle::getVertex1() const {
+    return vertex1;
+}
 
+Point Triangle::getVertex2() const {
+    return vertex2;
+}
 
+Point Triangle::getVertex3() const {
+    return vertex3;
+}
+
+Triangle::~Triangle() {}
