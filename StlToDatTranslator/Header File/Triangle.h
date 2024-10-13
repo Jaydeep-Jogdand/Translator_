755a262370823be8a7fee5ba77d6a0adc3df2356
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-13 18:08:08 +0530

    Update Triangle.h

--- StlToDatTranslator/Header File/Triangle.h
+++ StlToDatTranslator/Header File/Triangle.h
@@ -2,14 +2,19 @@
 #include "Point.h"
 
 class Triangle {
-public:
+private:  // Make Point objects private
     Point vertex1, vertex2, vertex3;
 
-
+public:
     Triangle(Point vertex1, Point vertex2, Point vertex3);
+
+    // Add getter methods for private members
+    Point getVertex1() const;
+    Point getVertex2() const;
+    Point getVertex3() const;
+
     ~Triangle();
 };
-
 
 
 
