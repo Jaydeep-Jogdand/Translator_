3cc10b0f3403911a01235bd6d36c2df73df6db89
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-13 18:10:44 +0530

    Update Writer.cpp
    Modify to use getter methods

--- StlToDatTranslator/Source File/Writer.cpp
+++ StlToDatTranslator/Source File/Writer.cpp
@@ -1,4 +1,3 @@
-
 #include "Writer.h"
 #include <fstream>
 #include <iomanip>
@@ -9,16 +8,13 @@ Writer::Writer() {}
 void Writer::saveToFile(const std::string& filePath, const std::vector<Triangle>& shapeTriangles) {
     std::ofstream file(filePath);
     for (const auto& tri : shapeTriangles) {
-   
-           file << tri.vertex1.getX() << " " << tri.vertex1.getY() << " " << tri.vertex1.getZ() << "\n"
-            << tri.vertex2.getX() << " " << tri.vertex2.getY() << " " << tri.vertex2.getZ() << "\n"
-            << tri.vertex3.getX() << " " << tri.vertex3.getY() << " " << tri.vertex3.getZ() << "\n"
-            << tri.vertex1.getX() << " " << tri.vertex1.getY() << " " << tri.vertex1.getZ() << "\n";
+        // Use getter methods to access vertex points
+        file << tri.getVertex1().getX() << " " << tri.getVertex1().getY() << " " << tri.getVertex1().getZ() << "\n"
+             << tri.getVertex2().getX() << " " << tri.getVertex2().getY() << " " << tri.getVertex2().getZ() << "\n"
+             << tri.getVertex3().getX() << " " << tri.getVertex3().getY() << " " << tri.getVertex3().getZ() << "\n"
+             << tri.getVertex1().getX() << " " << tri.getVertex1().getY() << " " << tri.getVertex1().getZ() << "\n";
     }
     std::cout << "Triangles saved to file: " << shapeTriangles.size() << std::endl;
 }
 
 Writer::~Writer() {}
-
-
-
