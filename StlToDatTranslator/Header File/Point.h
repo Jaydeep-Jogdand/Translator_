7a38d1f020509b788f933851e1fd315997bc7ea3
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-20 14:46:57 +0530

    change Position of Point Destructor

--- StlToDatTranslator/Header File/Point.h
+++ StlToDatTranslator/Header File/Point.h
@@ -2,16 +2,15 @@
 
 class Point {
 private:
-
+    
     int xPos, yPos, zPos;
 public:
     Point(int x, int y, int z);
+    ~Point();
     int getX() const;
     int getY() const;
     int getZ() const;
-
-
-    ~Point();
+    
 };
 
 
